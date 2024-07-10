0fdeccfc17f8f8b8ceca65093b35e2fd646cafc2
Jaromir Hradilek <jhradilek@gmail.com>
2024-07-10 23:55:34 +0200

    Added tests for discrete titles.

---
+++ test/test_floating_title.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require_relative 'helper'
+
+class FloatingTitleTest < Minitest::Test
+  def test_section_levels
+    xml = <<~EOF.chomp.to_dita
+    [discrete]
+    = Section 0
+
+    [discrete]
+    == Section 1
+
+    [discrete]
+    === Section 2
+
+    [discrete]
+    ==== Section 3
+
+    [discrete]
+    ===== Section 4
+
+    [discrete]
+    ====== Section 5
+    EOF
+
+    assert_xpath_includes xml, 'Section 0', '//p[@outputclass="title sect0"]/b/text()'
+    assert_xpath_includes xml, 'Section 1', '//p[@outputclass="title sect1"]/b/text()'
+    assert_xpath_includes xml, 'Section 2', '//p[@outputclass="title sect2"]/b/text()'
+    assert_xpath_includes xml, 'Section 3', '//p[@outputclass="title sect3"]/b/text()'
+    assert_xpath_includes xml, 'Section 4', '//p[@outputclass="title sect4"]/b/text()'
+    assert_xpath_includes xml, 'Section 5', '//p[@outputclass="title sect5"]/b/text()'
+  end
+end
