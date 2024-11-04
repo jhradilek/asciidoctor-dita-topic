6bda50fce5961070bc36dbdbb7f3c14668ea1b25
Jaromir Hradilek <jhradilek@gmail.com>
2024-11-05 00:48:09 +0100

    Added tests for the convert_listing() method.

--- test/test_listing.rb
+++ test/test_listing.rb
@@ -2,14 +2,74 @@ require 'minitest/autorun'
 require_relative 'helper'
 
 class ListingTest < Minitest::Test
-  def test_block_code
+  def test_explicit_listing_block
     xml = <<~EOF.chomp.to_dita
-    [source, ruby]
+    [listing]
+    A listing block
+    EOF
+
+    assert_xpath_equal xml, 'A listing block', '//codeblock/text()'
+  end
+
+  def test_delimited_listing_block
+    xml = <<~EOF.chomp.to_dita
     ----
-    Hello world
+    A listing block
     ----
     EOF
 
-    assert_xpath_equal xml, 'Hello world', '//codeblock/text()'
+    assert_xpath_equal xml, 'A listing block', '//codeblock/text()'
+  end
+
+  def test_listing_block_title
+    xml = <<~EOF.chomp.to_dita
+    .A listing block title
+    ----
+    A listing block
+    ----
+    EOF
+
+    assert_xpath_equal xml, 'A listing block title', '//p[@outputclass="title"]/b/text()'
+  end
+
+  def test_explicit_source_block
+    xml = <<~EOF.chomp.to_dita
+    [source]
+    A source code block
+    EOF
+
+    assert_xpath_equal xml, 'A source code block', '//codeblock/text()'
+  end
+
+  def test_delimited_source_block
+    xml = <<~EOF.chomp.to_dita
+    ----
+    A source code block
+    ----
+    EOF
+
+    assert_xpath_equal xml, 'A source code block', '//codeblock/text()'
+  end
+
+  def test_implied_source_block
+    xml = <<~EOF.chomp.to_dita
+    [,ruby]
+    ----
+    puts "Hello"
+    ----
+    EOF
+
+    assert_xpath_equal xml, 'puts "Hello"', '//codeblock/text()'
+  end
+
+  def test_source_block_language
+    xml = <<~EOF.chomp.to_dita
+    [source,ruby]
+    ----
+    puts "Hello"
+    ----
+    EOF
+
+    assert_xpath_equal xml, 'language-ruby', '//codeblock/@outputclass'
   end
 end
