a8fb91c4b4cba017c6fe88ecc9b655ec05273f52
Jaromir Hradilek <jhradilek@gmail.com>
2024-11-12 02:24:31 +0100

    Added tests for the convert_example() method.

---
+++ test/test_example.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require_relative 'helper'
+
+class ExampleTest < Minitest::Test
+  def test_explicit_example_block
+    xml = <<~EOF.chomp.to_dita
+    [example]
+    An example block
+    EOF
+
+    assert_xpath_equal xml, 'An example block', '//example/text()'
+  end
+
+  def test_delimited_example_block
+    xml = <<~EOF.chomp.to_dita
+    ====
+    An example block
+    ====
+    EOF
+
+    assert_xpath_equal xml, 'An example block', '//example/p/text()'
+  end
+
+  def test_example_block_id
+    xml = <<~EOF.chomp.to_dita
+    [#example-id]
+    ====
+    An example block
+    ====
+    EOF
+
+    assert_xpath_equal xml, 'example-id', '//example/@id'
+  end
+
+  def test_example_block_title
+    xml = <<~EOF.chomp.to_dita
+    .An example block title
+    ====
+    An example block
+    ====
+    EOF
+
+    assert_xpath_equal xml, 'An example block title', '//example/title/text()'
+    assert_xpath_equal xml, 'An example block', '//example/p/text()'
+  end
+end
