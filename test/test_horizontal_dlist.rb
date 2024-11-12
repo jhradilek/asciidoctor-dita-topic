0ac375a0a8c9db73fc3d77e10dac53eb482b5923
Jaromir Hradilek <jhradilek@gmail.com>
2024-11-13 00:45:19 +0100

    Added tests for the compose_horizontal_dlist() method.

---
+++ test/test_horizontal_dlist.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require_relative 'helper'
+
+class HorizontalDlistTest < Minitest::Test
+  def test_simple_horizontal_dlist
+    xml = <<~EOF.chomp.to_dita
+    [horizontal]
+    Term1:: Definition one
+    Term2:: Definition two
+    EOF
+
+    assert_xpath_equal xml, '2', '//table/tgroup/@cols'
+    assert_xpath_equal xml, 'Term1', '//table/tgroup/tbody/row[1]/entry[1]/b/text()'
+    assert_xpath_equal xml, 'Definition one', '//table/tgroup/tbody/row[1]/entry[2]/text()'
+    assert_xpath_equal xml, 'Term2', '//table/tgroup/tbody/row[2]/entry[1]/b/text()'
+    assert_xpath_equal xml, 'Definition two', '//table/tgroup/tbody/row[2]/entry[2]/text()'
+  end
+
+  def test_horizontal_dlist_outputclass
+    xml = <<~EOF.chomp.to_dita
+    [horizontal]
+    Term1:: Definition one
+    Term2:: Definition two
+    EOF
+
+    assert_xpath_equal xml, 'horizontal-dlist', '//table/@outputclass'
+  end
+
+  def test_horizontal_dlist_title
+    xml = <<~EOF.chomp.to_dita
+    .A description list title
+    [horizontal]
+    Term1:: Definition one
+    Term2:: Definition two
+    EOF
+
+    assert_xpath_equal xml, 'A description list title', '//table/title/text()'
+    assert_xpath_count xml, 2, '//table/tgroup/tbody/row'
+  end
+end
