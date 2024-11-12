ea02570703030fe71d68dfb315a6adbbe1f26a31
Jaromir Hradilek <jhradilek@gmail.com>
2024-11-12 01:33:29 +0100

    Added tests for the convert_olist() method.

---
+++ test/test_olist.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require_relative 'helper'
+
+class OlistTest < Minitest::Test
+  def test_simple_ordered_list
+    xml = <<~EOF.chomp.to_dita
+    . Item one
+    . Item two
+    EOF
+
+    assert_xpath_equal xml, 'Item one', '//ol/li[1]/text()'
+    assert_xpath_equal xml, 'Item two', '//ol/li[2]/text()'
+  end
+
+  def test_compound_ordered_list
+    xml = <<~EOF.chomp.to_dita
+    . Item one
+    +
+    Additional paragraph
+    . Item two
+    EOF
+
+    assert_xpath_equal xml, 'Item one', '//ol/li[1]/p[1]/text()'
+    assert_xpath_equal xml, 'Additional paragraph', '//ol/li[1]/p[2]/text()'
+  end
+
+  def test_ordered_list_title
+    xml = <<~EOF.chomp.to_dita
+    .An ordered list title
+    . Item one
+    . Item two
+    EOF
+
+    assert_xpath_equal xml, 'An ordered list title', '//p[@outputclass="title"]/b/text()'
+    assert_xpath_count xml, 2, '//ol/li'
+  end
+end
