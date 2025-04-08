9a716510291e4d3e74c56102fd6c9104a5bbefef
Jaromir Hradilek <jhradilek@gmail.com>
2025-04-08 23:35:57 +0200

    Added more tests for section IDs.

--- test/test_section.rb
+++ test/test_section.rb
@@ -17,4 +17,42 @@ class SectionTest < Minitest::Test
     assert_xpath_equal xml, 'Section title', '//section/title/text()'
     assert_xpath_equal xml, 'Section contents.', '//section/p/text()'
   end
+
+  def test_section_id_variants
+    xml = <<~EOF.chomp.to_dita
+    = Topic title
+
+    [#first-section]
+    == First section
+
+    Section contents.
+
+    [[second-section]]
+    == Second section
+
+    Section contents.
+
+    [id="third-section"]
+    == Third section
+
+    Section contents.
+    EOF
+
+    assert_xpath_count xml, 3, '//section'
+    assert_xpath_equal xml, 'first-section', '//section[1]/@id'
+    assert_xpath_equal xml, 'second-section', '//section[2]/@id'
+    assert_xpath_equal xml, 'third-section', '//section[3]/@id'
+  end
+
+  def test_auto_section_id
+    xml = <<~EOF.chomp.to_dita
+    = Topic title
+
+    == Section title
+
+    Section contents.
+    EOF
+
+    assert_xpath_equal xml, '_section_title', '//section/@id'
+  end
 end
