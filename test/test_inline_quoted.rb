55caaac0ccc487e99feeaf3d0ab5b63336dcd300
Jaromir Hradilek <jhradilek@gmail.com>
2025-04-29 01:34:04 +0200

    Added tests for inline STEM content.

--- test/test_inline_quoted.rb
+++ test/test_inline_quoted.rb
@@ -5,7 +5,7 @@ class InlineQuotedTest < Minitest::Test
   def test_markup_types
     xml = <<~EOF.chomp.to_dita
     Inline markup for _emphasis_, *strong*, `monospace`,
-    ^superscript^, ~subscript~, , and '`single quotes`'.
+    ^superscript^, and ~subscript~.
     EOF
 
     assert_xpath_equal xml, 'emphasis', '//i/text()'
@@ -29,5 +29,29 @@ class InlineQuotedTest < Minitest::Test
     EOF
 
     assert_xpath_equal xml, '&#8216;single quotes&#8217;', '//p/text()'
+  end
+
+  def test_asciimath
+    xml = <<~EOF.chomp.to_dita
+    :stem: asciimath
+    A line with inline STEM content: stem:[sqrt(16) = 4]
+    EOF
+
+    assert_xpath_count xml, 2, '//p/comment()'
+    assert_xpath_equal xml, 'asciimath start', '//p/comment()[1]'
+    assert_xpath_equal xml, 'asciimath end', '//p/comment()[2]'
+    assert_xpath_equal xml, 'sqrt(16) = 4', '//p/text()[2]'
+  end
+
+  def test_latexmath
+    xml = <<~EOF.chomp.to_dita
+    :stem: latexmath
+    A line with inline STEM content: stem:[\\alpha = \\beta + \\gamma]
+    EOF
+
+    assert_xpath_count xml, 2, '//p/comment()'
+    assert_xpath_equal xml, 'latexmath start', '//p/comment()[1]'
+    assert_xpath_equal xml, 'latexmath end', '//p/comment()[2]'
+    assert_xpath_equal xml, '\alpha = \beta + \gamma', '//p/text()[2]'
   end
 end
